5c745c1c82989001311ab831b91c1da0b9665e76
Rikard Funck <rikard.funck@stratiteq.com>
2023-10-15 22:48:04 +0200

    solved AOC01 part 2

--- AOC01/Program.cs
+++ AOC01/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("AOC 01:");
+// #1
+Console.WriteLine("AOC 01#1:");
 
 var frequency = 0;
-var path = Path.Combine(Directory.GetCurrentDirectory(), "Input.txt");
+var path = Path.Combine(Directory.GetCurrentDirectory(), "Data/input.txt");
 var frequencyChanges = File.ReadAllText(path).Split("\r\n");
 
-foreach (var frequencyChange in frequencyChanges) 
+foreach (var frequencyChange in frequencyChanges)
 {
     if (int.TryParse(frequencyChange, out int result))
     {
@@ -14,3 +15,36 @@ foreach (var frequencyChange in frequencyChanges)
 }
 
 Console.WriteLine($"Result: {frequency}");
+Console.WriteLine("----------------------------------");
+
+// #2
+Console.WriteLine("AOC 01#2:");
+
+frequency = 0;
+var frequencies = new List<int>();
+var hasOccuredTwice = false;
+
+while (!hasOccuredTwice)
+{
+    foreach (var frequencyChange in frequencyChanges)
+    {
+        if (int.TryParse(frequencyChange, out int result))
+        {
+            frequency += result;
+
+            if (frequencies.Contains(frequency))
+            {
+                hasOccuredTwice = true;
+            }
+
+            frequencies.Add(frequency);
+
+            if (hasOccuredTwice)
+            {
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Result: {frequencies.LastOrDefault()}");
