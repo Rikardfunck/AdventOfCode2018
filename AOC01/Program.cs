a5aa149a093b1918f8d9f5977cd614768e6dc1b2
Rikard Funck <rikard.funck@stratiteq.com>
2023-10-15 21:55:12 +0200

    solved AOC01

---
+++ AOC01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AOC 01:");
+
+var frequency = 0;
+var path = Path.Combine(Directory.GetCurrentDirectory(), "Input.txt");
+var frequencyChanges = File.ReadAllText(path).Split("\r\n");
+
+foreach (var frequencyChange in frequencyChanges) 
+{
+    if (int.TryParse(frequencyChange, out int result))
+    {
+        frequency += result;
+    }
+}
+
+Console.WriteLine($"Result: {frequency}");
