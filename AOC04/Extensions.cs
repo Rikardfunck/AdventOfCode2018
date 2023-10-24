5eeb235fff72fbec3526cba5420777fc3e0e061b
Rikard Funck <rikard.funck@stratiteq.com>
2023-10-24 14:25:21 +0200

    aoc4 part 1 solution

---
+++ AOC04/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AOC04;
+
+public static class Extensions
+{
+    public static IEnumerable<DataEntry> RawInputToListOfObjects(this string[] rawInput)
+    {
+        var dataEntries = new List<DataEntry>();
+        foreach (var dataEntryString in rawInput)
+        {
+            dataEntries.Add(dataEntryString.ConvertToDataObject()!);
+        }
+
+        return dataEntries.AsEnumerable();
+    }
+
+    private static DataEntry? ConvertToDataObject(this string rawData)
+    {
+        var data = rawData.Split("] ");
+
+        data[0] = data[0]
+            .Replace("[", string.Empty)
+            .Replace("]", string.Empty);
+
+        if (!DateTime.TryParse(data[0], out var result))
+            return default;
+
+        return (data[1].Contains("begins shift"), data[1].Contains("falls asleep"), data[1].Contains("wakes up")) switch
+        {
+            (true, _, _) => ConstructDataEntry(result, data[1], true, false, false, true),
+            (_, true, _) => ConstructDataEntry(result, data[1], false, false, true, false),
+            (_, _, true) => ConstructDataEntry(result, data[1], false, true, false, false),
+            _ => throw new InvalidDataException()
+        };
+    }
+
+    private static DataEntry ConstructDataEntry(DateTime date, string dataString, bool includeId, bool wakesUp, bool fallsAsleep, bool beginsShift)
+    {
+        var id = string.Empty;
+        if (includeId)
+        {
+            id = dataString.Split(" ")[1].Remove(0, 1);
+        }
+
+        return new DataEntry()
+        {
+            Date = date,
+            GuardId = id,
+            WakesUp = wakesUp,
+            FallsAsleep = fallsAsleep,
+            BeginsShift = beginsShift
+        };
+    }
+}
+
+public record DataEntry
+{
+    public DateTime Date { get; set; }
+
+    public string GuardId { get; set; }
+
+    public bool WakesUp { get; set; }
+
+    public bool FallsAsleep { get; set; }
+
+    public bool BeginsShift { get; set; }
+}
---
+++ AOC04/Program.cs
@@ -0,0 +1,64 @@
+﻿using AOC04;
+using Shared;
+
+Console.WriteLine("AOC04 #1:");
+
+var input = SharedHelpers
+    .ReadContentFromInputFile()
+    .RawInputToListOfObjects();
+
+var sortedInput = input.OrderBy(s => s.Date).ToList();
+var currentGuardId = string.Empty;
+
+for (int i = 0; i < sortedInput.Count; i++)
+{
+    if (!string.IsNullOrEmpty(sortedInput[i].GuardId))
+    {
+        currentGuardId = sortedInput[i].GuardId;
+    }
+
+    sortedInput[i].GuardId = currentGuardId;
+}
+
+var groupedSchedulesOnGuardId = sortedInput.GroupBy(item => item.GuardId).ToList();
+var guardAndSleptTime = new Dictionary<string, GuardData>();
+
+foreach (var group in groupedSchedulesOnGuardId)
+{
+    var minutesSleeping = new List<int>();
+    var minutesAsleep = 0;
+    
+    for (int i = 0; i < group.Count(); i++)
+    {
+        var currentGroup = group.ToList();
+        if (currentGroup[i].FallsAsleep)
+        {
+            var diff = currentGroup[i + 1].Date - currentGroup[i].Date;
+
+            for (int j = currentGroup[i].Date.Minute; j < currentGroup[i].Date.AddMinutes(diff.Minutes).Minute; j++)
+            {
+                minutesSleeping.Add(j);
+                minutesAsleep++;
+            }
+        }
+    }
+
+    var mostSleptMinute = minutesSleeping.ToArray().GroupBy(minute => minute).OrderByDescending(s => s.Count()).FirstOrDefault();
+    guardAndSleptTime[group.Key] = mostSleptMinute == null ? new GuardData() : new GuardData
+    {
+        MinuteMostAsleep = mostSleptMinute.Key,
+        MinutesAsleep = minutesAsleep
+    };
+}
+
+var mostAsleepGuard = guardAndSleptTime.OrderByDescending(s => s.Value.MinutesAsleep).First();
+
+Console.WriteLine($"The guard with ID: {mostAsleepGuard.Key} was asleep the most. He loved sleeping a little bit extra in minute {mostAsleepGuard.Value.MinuteMostAsleep}");
+Console.WriteLine($"Result: {int.Parse(mostAsleepGuard.Key) * mostAsleepGuard.Value.MinuteMostAsleep}");
+
+record GuardData
+{
+    public int MinuteMostAsleep { get; set; }
+
+    public int MinutesAsleep { get; set; }
+}
