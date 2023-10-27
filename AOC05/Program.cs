49a30a39f29920ce85f017d5ab78444cd435e64c
Rikard Funck <rikard.funck@stratiteq.com>
2023-10-27 11:41:43 +0200

    initial solution aoc05 part 1

--- AOC05/Program.cs
+++ AOC05/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shared;
+
+Console.WriteLine("AOC05 #1:");
+
+var input = SharedHelpers.ReadContentAndSplitFromInputFile(separator: "").First();
+var wholeStringProcessed = false;
+
+// Is correct but need to optimize this as much as possible
+while (!wholeStringProcessed)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input.ElementAtOrDefault(i + 1) != default(char))
+        {
+            var upperCaseCurrent = Char.ToUpper(input[i]);
+            var upperCaseNext = Char.ToUpper(input[i + 1]);
+
+            if (upperCaseCurrent == upperCaseNext)
+            {
+                if (Char.IsUpper(input[i]) != Char.IsUpper(input[i + 1]))
+                {
+                    input = input.Remove(i, 2);
+                    Console.WriteLine($"String lenght: {input.Length}");
+
+                    break;
+                }
+            }
+        }
+
+        if (i == (input.Length - 1))
+        {
+            wholeStringProcessed = true;
+        }
+    }
+}
+
+Console.WriteLine($"Result: {input}");
+
+/*
+ * Make a method which implements the removing logic
+ * Loop through each character in the string and check against the next one if its not the last
+ * When an alternation of the string happens, mutate and restart loop
+ */
